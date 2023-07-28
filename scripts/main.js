2892e72338e825c384def4da5d8813b1e37ea067
rhoda437 <yabukeero@gmail.com>
2023-07-28 18:39:40 -0400

    cosas

--- scripts/main.js
+++ scripts/main.js
@@ -10,25 +10,3 @@ miImage.onclick = function () {
 
 let miBoton = document.querySelector("button");
 let miTitulo = document.querySelector("h1");
-
-function estableceNombreUsuario() {
-  let miNombre = prompt("Introduzca su nombre.");
-  if (!miNombre) {
-    estableceNombreUsuario();
-  } else {
-    localStorage.setItem("nombre", miNombre);
-    miTitulo.innerHTML = "Bienvenido, " + miNombre;
-  }
-}
-
-
-if (!localStorage.getItem("nombre")) {
-  estableceNombreUsuario();
-} else {
-  let nombreAlmacenado = localStorage.getItem("nombre");
-  miTitulo.textContent = "Bienvenido," + nombreAlmacenado;
-}
-
-miBoton.onclick = function () {
-  estableceNombreUsuario();
-};
